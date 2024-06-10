24097a2d6181691ed10a60bf680590be6a66e829
Philippe Fanaro <philippefanaro@gmail.com>
2024-06-10 10:27:11 -0300

    first basic schema version

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,34 +1,165 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm"
 import {
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
-} from "drizzle-orm/pg-core";
+} from "drizzle-orm/pg-core"
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `googol_${name}`);
+import { standardNanoid } from "./nanoid"
 
-export const posts = createTable(
-  "post",
-  {
+export const createTable = pgTableCreator(
+  (name) => `googol_${name}`,
+)
+
+function idCols() {
+  return {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
+    nanoId: varchar("nano_id", { length: 256 })
+      .unique()
+      .notNull()
+      .$defaultFn(() => standardNanoid()),
+  }
+}
+
+function dateTimeCols() {
+  return {
+    createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
+    updatedAt: timestamp("updated_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  }
+}
+
+function imageUrlCol() {
+  return {
+    imageUrl: varchar("image_url", { length: 1024 }),
+  }
+}
+
+export const users = createTable(
+  "users",
+  {
+    // IDs
+    ...idCols(),
+    clerkId: varchar("clerk_id", { length: 256 })
+      .unique()
+      .notNull(),
+    username: varchar("username", { length: 256 })
+      .unique()
+      .notNull(),
+    email: varchar("email", { length: 256 })
+      .unique()
+      .notNull(),
+    // DB Metadata
+    ...dateTimeCols(),
+    // Data
+    firstName: varchar("first_name", { length: 256 }),
+    lastName: varchar("last_name", { length: 256 }),
+    ...imageUrlCol(),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+  (table) => ({
+    nanoidIdx: index("nano_id_idx").on(table.nanoId),
+    usernameIdx: index("username_idx").on(table.username),
+    clerkIdIdx: index("clerk_id_idx").on(table.clerkId),
+  }),
+)
+
+export const usersRelations = relations(
+  users,
+  ({ many }) => ({
+    links: many(links),
+    votes: many(votes),
+    // comments: many(comments),
+  }),
+)
+
+export const links = createTable(
+  "links",
+  {
+    // IDs
+    ...idCols(),
+    // DB Metadata
+    ...dateTimeCols(),
+    // Data
+    link: varchar("first_name", { length: 4096 }),
+    clicks: integer("clicks"),
+    // Relationships
+    creatorId: integer("creator_id").notNull(),
+  },
+  (table) => ({
+    nanoidIdx: index("nano_id_idx").on(table.nanoId),
+    createdAtIdx: index("created_at_idx").on(
+      table.createdAt,
+    ),
+    updatedAtIdx: index("updated_at_idx").on(
+      table.updatedAt,
+    ),
+  }),
+)
+
+export const linksRelations = relations(
+  links,
+  ({ one }) => ({
+    creator: one(users, {
+      fields: [links.creatorId],
+      references: [users.id],
+    }),
+  }),
+)
+
+export const votes = createTable("votes", {
+  // IDs
+  ...idCols(),
+  // Metadata
+  ...dateTimeCols(),
+  // Data
+  points: integer("points").notNull(),
+  // Relationships
+  voterId: integer("voter_id").notNull(),
+  linkId: varchar("link_id"),
+})
+
+export const votesRelations = relations(
+  votes,
+  ({ one }) => ({
+    creator: one(users, {
+      fields: [votes.voterId],
+      references: [users.id],
+    }),
+    link: one(links, {
+      fields: [votes.linkId],
+      references: [links.id],
+    }),
+  }),
+)
+
+export const comments = createTable("comments", {
+  // IDs
+  ...idCols(),
+  // Metadata
+  ...dateTimeCols(),
+  // Data
+  content: varchar("content", { length: 4096 }).notNull(),
+  // Relationships
+  commenterId: integer("commenter_id").notNull(),
+  linkId: varchar("link_id"),
+})
+
+export const commentsRelations = relations(
+  comments,
+  ({ one }) => ({
+    creator: one(users, {
+      fields: [comments.commenterId],
+      references: [users.id],
+    }),
+    node: one(links, {
+      fields: [comments.linkId],
+      references: [links.id],
+    }),
+  }),
+)
