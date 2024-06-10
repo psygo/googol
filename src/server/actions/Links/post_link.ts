51803bc07e540eb0a9182ce3c36d6c71710a4e67
Philippe Fanaro <philippefanaro@gmail.com>
2024-06-10 12:57:04 -0300

    first actions setup

---
+++ src/server/actions/Links/post_link.ts
@@ -0,0 +1,11 @@
+// import { db, links } from "@db"
+
+export async function postLink() {
+  try {
+    // await db.insert(links).values({
+    //   link: "https://google.com",
+    // })
+  } catch (e) {
+    console.error(e)
+  }
+}
---
+++ src/server/db/exports.ts
@@ -0,0 +1,3 @@
+export * from "./"
+export * from "./nanoid"
+export * from "./schema"
